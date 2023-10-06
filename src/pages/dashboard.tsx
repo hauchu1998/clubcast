d541cd6dbfcab3a25ea1bef5ee5e689d9e5338fb
Hau Chu <hchoward1998@gmail.com>
2023-10-05 21:56:34 -0400

    and ensName as well

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,15 +1,16 @@
-import { useAccount } from "wagmi";
-import { useEnsName } from "wagmi";
+import { useAccount, useEnsName } from "wagmi";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import BaseApp from "@/components/baseApp";
-import type { address } from "@/types/address";
 import Blockies from "react-blockies";
+import { isValidLongWalletAddress } from "@/helpers/address";
+import { address } from "@/types/address";
 
 const Dashboard = () => {
   const { address: walletAddress, isConnected } = useAccount();
   const { data: ensName } = useEnsName({
     address: walletAddress as address,
+    enabled: isValidLongWalletAddress(walletAddress as address),
+    chainId: 1,
   });
   const router = useRouter();
   useEffect(() => {
@@ -18,30 +19,38 @@ const Dashboard = () => {
     }
   }, [isConnected, router]);
   return (
-    <BaseApp>
-      <div className="w-full py-3">
-        <div className="w-full px-10 py-3 flex gap-5 items-center">
-          <div
+    <div className="w-full py-3">
+      <div className="w-full px-10 py-3 flex gap-5 items-center">
+        <div data-testid="avatar" className="flex items-center justify-center">
+          <Blockies
             data-testid="avatar"
-            className="flex items-center justify-center"
-          >
-            <Blockies
-              data-testid="avatar"
-              seed={walletAddress?.toLowerCase() || "default_profile"}
-              scale={5}
-              size={20}
-              className="rounded-full"
-            />
-          </div>
-          <div className="font-bold">
-            <div className=" text-2xl">{walletAddress}</div>
-            <button className="mt-1 bg-blue-400 px-4 py-1 text-white rounded-full">
-              {ensName ?? "register ENS"}
-            </button>
-          </div>
+            seed={walletAddress?.toLowerCase() || "default_profile"}
+            scale={5}
+            size={20}
+            className="rounded-full"
+          />
+        </div>
+        <div className="font-bold">
+          <div className=" text-2xl">{walletAddress}</div>
+          {
+            // @ts-ignore
+            ensName ? (
+              <div className="mt-1 bg-blue-400 px-4 py-1 text-white rounded-full">
+                {ensName}
+              </div>
+            ) : (
+              <a
+                className="mt-1 bg-blue-400 px-4 py-1 text-white rounded-full"
+                href="https://ens.domains/"
+                target="_blank"
+              >
+                register ENS
+              </a>
+            )
+          }
         </div>
       </div>
-    </BaseApp>
+    </div>
   );
 };
 
